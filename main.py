1737ffd221d18c90fa2626953ae8fa57728b29e3
vicarious11 <saksham.dutta@smartjoules.in>
2020-09-17 02:19:18 +0530

    added

--- main.py
+++ main.py
@@ -45,50 +45,50 @@ def reset_app_layout(list_of_input_images, list_of_output_images):
                 width=6
             )
         ),
-        dbc.Row([
-            dbc.Col(
-                html.Div([
-                    dcc.Upload(
-                        id='upload-input-image',
-                        children=html.Div([
-                            'Drag and Drop Input Image'
-                        ]),
-                        style={
-                            'width': '100%',
-                            'height': '60px',
-                            'lineHeight': '60px',
-                            'borderWidth': '1px',
-                            'borderStyle': 'dashed',
-                            'borderRadius': '5px',
-                            'textAlign': 'center',
-                            'margin': '10px'
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True
-                    ),
-                    html.Div(id='input-image-upload')]), width={'size': 4, 'offset': 1}),
-
-            dbc.Col(
-                html.Div([
-                    dcc.Upload(
-                        id='upload-output-image',
-                        children=html.Div([
-                            'Drag and Drop Output Image'
-                        ]),
-                        style={
-                            'width': '100%',
-                            'height': '60px',
-                            'lineHeight': '60px',
-                            'borderWidth': '1px',
-                            'borderStyle': 'dashed',
-                            'borderRadius': '5px',
-                            'textAlign': 'center',
-                            'margin': '10px'
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True
-                    ),
-                    html.Div(id='output-image-upload')]), width={'size': 4, 'offset': 1})]),
+        # dbc.Row([
+        #     dbc.Col(
+        #         html.Div([
+        #             dcc.Upload(
+        #                 id='upload-input-image',
+        #                 children=html.Div([
+        #                     'Drag and Drop Input Image'
+        #                 ]),
+        #                 style={
+        #                     'width': '100%',
+        #                     'height': '60px',
+        #                     'lineHeight': '60px',
+        #                     'borderWidth': '1px',
+        #                     'borderStyle': 'dashed',
+        #                     'borderRadius': '5px',
+        #                     'textAlign': 'center',
+        #                     'margin': '10px'
+        #                 },
+        #                 # Allow multiple files to be uploaded
+        #                 multiple=True
+        #             ),
+        #             html.Div(id='input-image-upload')]), width={'size': 4, 'offset': 1}),
+        #
+        #     dbc.Col(
+        #         html.Div([
+        #             dcc.Upload(
+        #                 id='upload-output-image',
+        #                 children=html.Div([
+        #                     'Drag and Drop Output Image'
+        #                 ]),
+        #                 style={
+        #                     'width': '100%',
+        #                     'height': '60px',
+        #                     'lineHeight': '60px',
+        #                     'borderWidth': '1px',
+        #                     'borderStyle': 'dashed',
+        #                     'borderRadius': '5px',
+        #                     'textAlign': 'center',
+        #                     'margin': '10px'
+        #                 },
+        #                 # Allow multiple files to be uploaded
+        #                 multiple=True
+        #             ),
+        #             html.Div(id='output-image-upload')]), width={'size': 4, 'offset': 1})]),
         dbc.Row(
             [
                 dbc.Col(dcc.Dropdown(
